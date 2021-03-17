27272ea600a5d09d8a555cce1a3ab540172f9f1a
bioshiki <55005595+bioshiki@users.noreply.github.com>
2021-03-18 05:50:18 +0900

    Update 268.cpp

--- 0200-0299/268.cpp
+++ 0200-0299/268.cpp
@@ -1,3 +1,5 @@
+//runtime O(n), space O(1)
+//method 1
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
@@ -18,3 +20,18 @@ public:
         return n;
     }
 };
+
+/*
+//method 2:
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        return n*(n+1)/2-sum;
+    }
+};
+*/
